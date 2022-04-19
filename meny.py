424a420486cb5122c54ef443ae5bcc0d1ba9e411
André Mathisen <9752808+anzure@users.noreply.github.com>
2022-04-19 15:08:18 +0200

    Program meny

---
+++ meny.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+ 
+root = tk.Tk()
+root.configure(background='dark green')
+root.title("Meny")
+root.geometry("500x300")
+
+heading = tk.Label(root, text="Meny", bg="dark green")
+heading.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+
+button1 = tk.Button(root, text="Lån av utstyr", fg="Black", bg="Grey")
+button2 = tk.Button(root, text="Legge til nytt komponent", fg="Black", bg="Grey")
+button1.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+button2.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+ 
+root.mainloop()
