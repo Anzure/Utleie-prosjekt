8259eab1d42b809c246433fafba94deb825d41d8
Rudi Abrahamsen <47385921+rudabrah@users.noreply.github.com>
2022-04-20 09:42:49 +0200

    Update Legginn.py
    Forbedret create table kommandoer så tabeller er riktig formatert. Jobber med å få inn ny komponenter, men behøver riktig tk-referanser fra GUI

--- Legginn.py
+++ Legginn.py
@@ -14,13 +14,21 @@ print("Connected to database.")
 mycursor = database.cursor()
 
 mycursor.execute(
-    "CREATE TABLE IF NOT EXISTS Komponenter (KomponentID int, Navn VARCHAR(45), Beholdning int, PRIMARY KEY(KomponentID))"
+    "CREATE TABLE IF NOT EXISTS Komponenter (KomponentID int auto_increment PRIMARY KEY NOT NULL, Navn VARCHAR(45), Beholdning int)"
 )
 mycursor.execute(
-    "CREATE TABLE IF NOT EXISTS Lån (idLån int, komponentID int, LånerID int, PRIMARY KEY(idLån))"
+    "CREATE TABLE IF NOT EXISTS Lån (idLån int auto_increment PRIMARY KEY NOT NULL, komponentID int, LånerID int)"
 )
 mycursor.execute(
-    "CREATE TABLE IF NOT EXISTS Leier (LånerID int, Navn varchar(45), Utlånsdato date, leveringsfrist date, Epost varchar(45), TLF int, PRIMARY KEY(LånerID))"
+    "CREATE TABLE IF NOT EXISTS Leier (LånerID int auto_increment PRIMARY KEY NOT NULL, Navn varchar(45), Utlånsdato date, leveringsfrist date, Epost varchar(45), TLF int)"
 )
 
 print("Execution complete")
+
+
+# Tenkt modell for funksjoner som skal hente info fra gui
+# def nyKompNavn():
+#    NyKomp = navn.get()
+
+
+nykomp = "INSERT INTO Komponenter (Navn) VALUER (%s)"  # Grunnlag for kommando som skal legge inn nytt komponent. ID har autoinc og ordner seg selv.
