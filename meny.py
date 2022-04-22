fd7390ab66dc11115e3aa2fe34eabec76ec87a4b
André Mathisen <9752808+anzure@users.noreply.github.com>
2022-04-22 09:44:43 +0200

    Diverse endringer

--- meny.py
+++ meny.py
@@ -1,4 +1,11 @@
 import tkinter as tk
+import sys
+import threading
+
+
+
+
+
  
 root = tk.Tk()
 root.configure(background='dark green')
@@ -8,8 +15,14 @@ root.geometry("500x250")
 heading = tk.Label(root, text="Meny", bg="dark green")
 heading.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
 
-button1 = tk.Button(root, text="Lån av utstyr", fg="Black", bg="Grey")
-button2 = tk.Button(root, text="Legge til nytt komponent", fg="Black", bg="Grey")
+def command1():
+    exec(open("utleie.py").read())
+
+def command2():
+    exec(open("Legginn.py").read())
+
+button1 = tk.Button(root, text="Lån av utstyr", fg="Black", bg="Grey", command=command1)
+button2 = tk.Button(root, text="Legge til nytt komponent", fg="Black", bg="Grey", command=command2)
 button1.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 button2.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
  
--- utleie.py
+++ utleie.py
@@ -123,20 +123,47 @@ def insert():
             clear()
             messagebox.showinfo("Suksess", "Utleie registrert")
 
+def add_component():
+    knavn = in_knavn.get()
+    kAnt = in_kAnt.get()
 
-if __name__ == "__main__":
-     
-    root = tk.Tk()
-    root.configure(background='dark green')
-    root.title("Registrer utlån")
-    root.geometry("500x250")
+    if knavn == "" or kAnt == "" or kAnt == "0":
+        messagebox.showinfo(
+            title="Feil", message="Fyll alle felt. Antall må være 1 eller fler")
+    else:
+        cursor.execute(
+            "INSERT INTO komponenter(Navn, Beholdning) VALUES ('" + knavn + "','" + kAnt + "')")
+        cursor.execute("commit")
+        messagebox.showinfo(title="Info", message="Komponent registrert")
+
+
+def close_rental_window():
+    root.deiconify()
+    rental.withdraw()
+    
+def open_rental_window():
+    root.withdraw()
+    rental.deiconify()
+
+def close_register_window():
+    root.deiconify()
+    register.withdraw()
+    
+def open_register_window():
+    root.withdraw()
+    register.deiconify()
+
+def contruct_rental():
+    rental.configure(background='dark green')
+    rental.title("Registrer utlån")
+    rental.geometry("500x250")
  
-    heading = tk.Label(root, text="Utlånsskjema", bg="dark green")
-    name_label = tk.Label(root, text="Navn", bg="dark green")
-    phone_label = tk.Label(root, text="Telefonnummer", bg="dark green")
-    email_label = tk.Label(root, text="E-postadresse", bg="dark green")
-    component_label = tk.Label(root, text="Komponent", bg="dark green")
-    deadline_label = tk.Label(root, text="Leveringsfrist", bg="dark green")
+    heading = tk.Label(rental, text="Utlånsskjema", bg="dark green")
+    name_label = tk.Label(rental, text="Navn", bg="dark green")
+    phone_label = tk.Label(rental, text="Telefonnummer", bg="dark green")
+    email_label = tk.Label(rental, text="E-postadresse", bg="dark green")
+    component_label = tk.Label(rental, text="Komponent", bg="dark green")
+    deadline_label = tk.Label(rental, text="Leveringsfrist", bg="dark green")
     
     heading.grid(row=0, column=1)
     name_label.grid(row=1, column=0)
@@ -145,13 +172,13 @@ if __name__ == "__main__":
     component_label.grid(row=4, column=0)
     deadline_label.grid(row=5, column=0)
     
-    name_field = tk.Entry(root)
-    phone_field = tk.Entry(root)
-    email_field = tk.Entry(root)
-    component_name = tk.StringVar(root)
+    name_field = tk.Entry(rental)
+    phone_field = tk.Entry(rental)
+    email_field = tk.Entry(rental)
+    component_name = tk.StringVar(rental)
     component_name.set(component_names[0])
-    component_field = tk.OptionMenu(root, component_name, *component_names)
-    deadline_field = tk.Entry(root)
+    component_field = tk.OptionMenu(rental, component_name, *component_names)
+    deadline_field = tk.Entry(rental)
 
     name_field.bind("<Return>", focus1)
     phone_field.bind("<Return>", focus2)
@@ -165,7 +192,56 @@ if __name__ == "__main__":
     component_field.grid(row=4, column=1, ipadx="100")
     deadline_field.grid(row=5, column=1, ipadx="100")
     
-    submit = tk.Button(root, text="Registrer utlån", fg="Black",
+    submit = tk.Button(rental, text="Registrer utlån", fg="Black",
                             bg="Grey", command=insert)
     submit.grid(row=6, column=1)
+
+    rental.protocol("WM_DELETE_WINDOW", close_rental_window)
+    rental.withdraw()
+
+def construct_register():
+    register.configure(background="dark green")
+    register.geometry("600x300")
+    register.title("Registrer ny komponent")
+
+    knavn = tk.Label(register, text="Komponentnavn", font=("bold", 10), bg="dark green")
+    knavn.place(x=30, y=50)
+
+    kAnt = tk.Label(register, text="Antall enheter: ", font=("bold", 10), bg="dark green")
+    kAnt.place(x=30, y=80)
+
+    in_knavn = tk.Entry(register)
+    in_knavn.place(x=160, y=50)
+
+    in_kAnt = tk.Entry(register)
+    in_kAnt.place(x=160, y=80)
+
+    register_submit = tk.Button(register, text="Registrer", fg="Black",
+                    bg="Grey", command=insert)
+    register_submit.place(x=90, y=200)
+    
+    register.protocol("WM_DELETE_WINDOW", close_register_window)
+    register.withdraw()
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.configure(background='dark green')
+    root.title("Meny")
+    root.geometry("500x250")
+
+    heading = tk.Label(root, text="Meny", bg="dark green")
+    heading.place(relx=0.5, rely=0.2, anchor=tk.CENTER)
+
+    button1 = tk.Button(root, text="Lån av utstyr", fg="Black", bg="Grey", command=open_rental_window)
+    button2 = tk.Button(root, text="Legge til nytt komponent", fg="Black", bg="Grey", command=open_register_window)
+    button1.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+    button2.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+
+    rental = tk.Toplevel(root)
+    register = tk.Toplevel(root)
+    contruct_rental()
+    construct_register()
+    
     root.mainloop()
+    
